e28ea54ebedf386bd15812650bd00d0a597a33dd
duc anh <nducanh1010@gmail.com>
2024-01-11 10:16:08 +0700

    css board content

--- src/theme.js
+++ src/theme.js
@@ -5,7 +5,9 @@ import { experimental_extendTheme as extendTheme } from "@mui/material/styles";
  *  getInitColorSchemeScript () use for Nextjs
  * providing colorScheme hook to handle dark and light mode
  */
-
+const APP_BAR_HEIGHT = "58px";
+const BOARD_BAR_HEIGHT = "60px";
+const BOARD_CONTENT_HEIGHT = `calc(100vh - ${APP_BAR_HEIGHT} - ${BOARD_BAR_HEIGHT} )`;
 const theme = extendTheme({
   components: {
     MuiCssBaseline: {
@@ -43,6 +45,13 @@ const theme = extendTheme({
         },
       },
     },
+    MuiTypography: {
+      styleOverrides: {
+        root: {
+          "&.MuiTypography-body1": { fontSize: "0.875rem" },
+        },
+      },
+    },
     MuiOutlinedInput: {
       styleOverrides: {
         root: {
@@ -61,8 +70,9 @@ const theme = extendTheme({
     },
   },
   trello: {
-    appBarHeight: "58px",
-    boardBarHeight: "60px",
+    appBarHeight: APP_BAR_HEIGHT,
+    boardBarHeight: BOARD_BAR_HEIGHT,
+    boardContentHeight: BOARD_CONTENT_HEIGHT,
   },
   colorSchemes: { light: {}, dark: {} },
   // ...other properties
